4dea580a5170fbbd44e26456a2a344f161025e24
MifteeAlam <52493586+MifteeAlam@users.noreply.github.com>
2024-10-25 14:41:42 +0600

    Add files via upload

---
+++ Tuple[Day 6].py
@@ -0,0 +1,21 @@
+''' 100 days python challenge
+Day 6	Python Basics - Tuples	Create a tuple and demonstrate accessing elements by index.'''
+
+
+# Creating a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Accessing elements by index
+first_element = my_tuple[0]   # Accessing the first element
+second_element = my_tuple[1]  # Accessing the second element
+last_element = my_tuple[-1]   # Accessing the last element using a negative index
+
+# Printing the accessed elements
+print("First element:", first_element)
+print("Second element:", second_element)
+print("Last element:", last_element)
+
+
+
+# Printing the whole tuple
+print("The entire tuple:", my_tuple)
