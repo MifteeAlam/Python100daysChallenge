cea0dc22fe3d38261636192e11eb56cc9843be54
MifteeAlam <52493586+MifteeAlam@users.noreply.github.com>
2025-02-17 12:47:24 +0600

    Add files via upload

---
+++ Control Structures - while Loops[Day 11].py
@@ -0,0 +1,17 @@
+'''                  100 days python Challenge
+Day 11	Control Structures - While Loops	Use a while loop to print numbers from 1 to 10.'''
+
+
+# Initialize the variable to start from 1
+num = 1  
+
+# Use a while loop that runs as long as num is less than or equal to 10
+while num <= 10:  
+    # Print the current value of num
+    print(f"Current number: {num}")  
+    
+    # Increment num by 1 to move to the next number
+    num += 1  
+
+# After the loop ends, print a message indicating completion
+print("Loop has finished. All numbers from 1 to 10 have been printed.")
